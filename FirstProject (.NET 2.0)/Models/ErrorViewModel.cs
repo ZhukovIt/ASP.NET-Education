952a3c7b753d711c93a1d05709d7b91632c49b24
Victor Zhukov <v.zhukov@simplex48.ru>
2023-02-01 12:53:17 +0300

    Добавил .NET проект версии 2.0

---
+++ FirstProject (.NET 2.0)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstProject__.NET_2._0_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
