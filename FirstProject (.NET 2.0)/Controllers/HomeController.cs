789a463edc2eb26764e9f597a8f51e054d34ebdf
Victor Zhukov <v.zhukov@simplex48.ru>
2023-02-03 16:15:02 +0300

    Реализовал первый (промежуточный) вариант приложения

--- FirstProject (.NET 2.0)/Controllers/HomeController.cs
+++ FirstProject (.NET 2.0)/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using PartyInvites.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PartyInvites.Controllers
 {
@@ -6,7 +8,37 @@ namespace PartyInvites.Controllers
     {
         public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            if (hour > 6 && hour < 12)
+            {
+                ViewBag.Greeting = "Доброе утро";
+            }
+            else if (hour >= 12 && hour < 17)
+            {
+                ViewBag.Greeting = "Добрый день";
+            }
+            else if (hour >= 17 && hour < 22)
+            {
+                ViewBag.Greeting = "Добрый вечер";
+            }
+            else
+            {
+                ViewBag.Greeting = "Доброй ночи";
+            }
+            
             return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult FormGoParty()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult FormGoParty(GuestResponse guestResponse)
+        {
+            return View();
         }
     }
 }
