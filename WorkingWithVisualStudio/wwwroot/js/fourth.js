6d576ac2224fc9dfa43c7a1371c42f5db2bac36e
Victor Zhukov <v.zhukov@simplex48.ru>
2023-02-22 16:57:43 +0300

    Добавил в проект статическое содержимое

---
+++ WorkingWithVisualStudio/wwwroot/js/fourth.js
@@ -0,0 +1,5 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var element = document.createElement("p");
+    element.textContent = "Этот элемент создан в файле fourth.js";
+    document.querySelector("body").appendChild(element);
+});
