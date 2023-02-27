f2e49228d4128f1714e00c0f28188235eab1ee61
Victor Zhukov <v.zhukov@simplex48.ru>
2023-02-27 14:22:08 +0300

    Прогон первых модульных тестов

---
+++ WorkingWithVisualStudio.Tests/ProductTests.cs
@@ -0,0 +1,38 @@
+﻿using WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            // Организация
+            Product p = new Product
+            {
+                Name = "Test",
+                Price = 100M
+            };
+            // Действие
+            p.Name = "New Name";
+            //Утверждение
+            Assert.Equal("New Name", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            // Организация
+            Product p = new Product
+            {
+                Name = "Test",
+                Price = 100M
+            };
+            // Действие
+            p.Price = 200M;
+            // Утверждение
+            Assert.Equal(200M, p.Price);
+        }
+    }
+}
