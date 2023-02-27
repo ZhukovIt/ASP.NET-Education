f15a2c411347d1a5010b67b855b9349f0e2535fb
Victor Zhukov <v.zhukov@simplex48.ru>
2023-02-27 15:17:58 +0300

    Написал новый тест, который подтверждает, что ошибка контроллера наблюдается при цене больше 50

--- WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -8,15 +8,61 @@ namespace WorkingWithVisualStudio.Tests
 {
     public class HomeControllerTests
     {
+        class ModelCompleteFakeRepository : IRepository
+        {
+            public IEnumerable<Product> Products { get; } = new Product[]
+            {
+                new Product { Name = "P1", Price = 275M },
+                new Product { Name = "P2", Price = 48.95M },
+                new Product { Name = "P3", Price = 19.50M },
+                new Product { Name = "P3", Price = 34.95M }
+            };
+
+            public void AddProduct(Product p)
+            {
+                // Метод ничего не делает, так как этого не требуется для теста
+            }
+        }
+
         [Fact]
         public void IndexActionModelIsComplete()
         {
             // Организация
             HomeController controller = new HomeController();
+            controller.Repository = new ModelCompleteFakeRepository();
             // Действие
             IEnumerable<Product> model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
             // Утверждение
-            Assert.Equal(SimpleRepository.SharedRepository.Products, model,
+            Assert.Equal(controller.Repository.Products, model,
+                Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+
+        class ModelCompleteFakeRepositoryPricesUnder50 : IRepository
+        {
+            public IEnumerable<Product> Products { get; } = new Product[]
+            {
+                new Product { Name = "P1", Price = 5M },
+                new Product { Name = "P2", Price = 48.95M },
+                new Product { Name = "P3", Price = 19.50M },
+                new Product { Name = "P3", Price = 34.95M }
+            };
+
+            public void AddProduct(Product p)
+            {
+                // Метод ничего не делает, так как этого не требуется для теста
+            }
+        }
+
+        [Fact]
+        public void IndexActionModelIsCompletePricesUnder50()
+        {
+            // Организация
+            HomeController controller = new HomeController();
+            controller.Repository = new ModelCompleteFakeRepositoryPricesUnder50();
+            // Действие
+            IEnumerable<Product> model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+            // Утверждение
+            Assert.Equal(controller.Repository.Products, model,
                 Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
         }
     }
