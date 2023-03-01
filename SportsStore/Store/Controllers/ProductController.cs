742f62a14c63c102352f2fbccc305f050c6e9cd5
Victor Zhukov <v.zhukov@simplex48.ru>
2023-03-01 15:41:49 +0300

    Добавили поддержку разбиения товаров на страницы

--- SportsStore/Store/Controllers/ProductController.cs
+++ SportsStore/Store/Controllers/ProductController.cs
@@ -1,17 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Store.Models;
+using System.Linq;
 
 namespace Store.Controllers
 {
     public sealed class ProductController : Controller
     {
         private IProductRepository m_repository;
+        private int m_PageSize;
+
+        public int PageSize => m_PageSize;
 
         public ProductController(IProductRepository repository)
         {
             m_repository = repository;
+            m_PageSize = 4;
         }
 
-        public ViewResult List() => View(m_repository.Products);
+        public ViewResult List(int productPage = 1) =>
+            View(m_repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((productPage - 1) * m_PageSize)
+                .Take(m_PageSize)
+            );
     }
 }
