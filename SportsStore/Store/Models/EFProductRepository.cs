8598843acb01ebb0b7d13469dea9b0603fe7264c
Victor Zhukov <v.zhukov@simplex48.ru>
2023-03-01 13:20:57 +0300

    Добавили контекст БД от EFCore

---
+++ SportsStore/Store/Models/EFProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Store.Models
+{
+    public sealed class EFProductRepository : IProductRepository
+    {
+        private ApplicationDbContext m_context;
+
+        public EFProductRepository(ApplicationDbContext context)
+        {
+            m_context = context;
+        }
+
+        public IQueryable<Product> Products => m_context.Products;
+    }
+}
