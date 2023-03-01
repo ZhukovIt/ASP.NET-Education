d40bc235b744cc246ca4d1adf15033153052f3bc
Victor Zhukov <v.zhukov@simplex48.ru>
2023-03-01 10:07:31 +0300

    Добавил инфраструктуру имитации Moq

--- WorkingWithVisualStudio.Tests/HomeControllerTests.cs
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -3,31 +3,21 @@ using System.Collections.Generic;
 using WorkingWithVisualStudio.Controllers;
 using WorkingWithVisualStudio.Models;
 using Xunit;
+using System;
+using Moq;
 
 namespace WorkingWithVisualStudio.Tests
 {
     public class HomeControllerTests
     {
-        class ModelCompleteFakeRepository : IRepository
-        {
-            public IEnumerable<Product> Products { get; set; }
-
-            public void AddProduct(Product p)
-            {
-                // Метод ничего не делает, так как этого не требуется для теста
-            }
-        }
-
         [Theory]
         [ClassData(typeof(ProductTestData))]
         public void IndexActionModelIsComplete(Product[] products)
         {
             // Организация
-            HomeController controller = new HomeController();
-            controller.Repository = new ModelCompleteFakeRepository
-            {
-                Products = products
-            };
+            Mock<IRepository> mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(products);
+            HomeController controller = new HomeController { Repository = mock.Object };
             // Действие
             IEnumerable<Product> model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
             // Утверждение
@@ -35,34 +25,17 @@ namespace WorkingWithVisualStudio.Tests
                 Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
         }
 
-        class PropertyOnceFakeRepository : IRepository
+        [Fact]
+        public void RepositoryPropertyCalledOnce()
         {
-            public int PropertyCounter { get; set; } = 0;
-
-            public IEnumerable<Product> Products
-            {
-                get
-                {
-                    PropertyCounter++;
-                    return new[] { new Product { Name = "P1", Price = 100 } };
-                }
-            }
-
-            public void AddProduct(Product p)
-            {
-                // Метод ничего не делает, так как для теста это не требуется
-            }
-
-            public void RepositoryPropertyCalledOnce()
-            {
-                // Организация
-                PropertyOnceFakeRepository repo = new PropertyOnceFakeRepository();
-                HomeController controller = new HomeController { Repository = repo };
-                // Действие
-                IActionResult result = controller.Index();
-                // Утверждение
-                Assert.Equal(1, repo.PropertyCounter);
-            }
+            // Организация
+            Mock<IRepository> mock = new Mock<IRepository>();
+            mock.SetupGet(m => m.Products).Returns(new Product[] { new Product { Name = "P1", Price = 100 } });
+            HomeController controller = new HomeController { Repository = mock.Object };
+            // Действие
+            IActionResult result = controller.Index();
+            // Утверждение
+            mock.VerifyGet(m => m.Products, Times.Once);
         }
     }
 }
