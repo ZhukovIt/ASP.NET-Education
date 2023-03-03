f9024944edf9382588999c839b11c75b5bbbf0f0
Victor Zhukov <v.zhukov@simplex48.ru>
2023-03-03 17:22:20 +0300

    Сгенерировал частичное представление для получения списка категорий

---
+++ SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public sealed class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository m_repository;
+
+        public NavigationMenuViewComponent(IProductRepository repository)
+        {
+            m_repository = repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(m_repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
