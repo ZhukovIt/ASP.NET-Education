f23434c8489b6052ea5add3ea4eb79a1cfed378f
Victor Zhukov <v.zhukov@simplex48.ru>
2023-03-08 23:51:13 +0300

    Добавили виджет с итоговой информацией по корзине

---
+++ SportsStore/SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public sealed class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart m_cart;
+
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            m_cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(m_cart);
+        }
+    }
+}
