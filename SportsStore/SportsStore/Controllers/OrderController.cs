ca9cff8fb17e3aeb4b586281b5add1b4d270418d
Victor Zhukov <v.zhukov@simplex48.ru>
2023-03-10 09:19:05 +0300

    Добавили реализацию процесса оплаты заказа

---
+++ SportsStore/SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public sealed class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
