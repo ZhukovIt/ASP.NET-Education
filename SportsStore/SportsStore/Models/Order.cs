8813ef5c05bd46ba21b7e2f7c737cbeea686373c
Victor Zhukov <v.zhukov@simplex48.ru>
2023-03-10 08:56:35 +0300

    Определили программный интерфейс для класса заказа

---
+++ SportsStore/SportsStore/Models/Order.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SportsStore.Models
+{
+    public sealed class Order
+    {
+        [BindNever]
+        public int OrderID { get; set; }
+
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите Ваше имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите первую строку Вашего адреса")]
+        public string Line1 { get; set; }
+
+        public string Line2 { get; set; }
+
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите название Вашей страны")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите название Вашего города")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите название Вашего региона / области / штата")]
+        public string Region { get; set; }
+
+        public string Zip { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
