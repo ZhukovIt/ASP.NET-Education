167dfbfd46a8124efd135b1e4b03d9a380f5c801
Victor Zhukov <v.zhukov@simplex48.ru>
2023-03-10 10:04:22 +0300

    Создали хранилище заказов

---
+++ SportsStore/SportsStore/Models/EFOrderRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public sealed class EFOrderRepository : IOrderRepository
+    {
+        private ApplicationDbContext m_context;
+
+        public EFOrderRepository(ApplicationDbContext context)
+        {
+            m_context = context;
+        }
+
+        public IQueryable<Order> Orders => m_context.Orders
+            .Include(order => order.Lines)
+            .ThenInclude(line => line.Product);
+
+        public void SaveOrder(Order order)
+        {
+            m_context.AttachRange(order.Lines.Select(line => line.Product));
+            if (order.OrderID == 0)
+            {
+                m_context.Orders.Add(order);
+            }
+            m_context.SaveChanges();
+        }
+    }
+}
