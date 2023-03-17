37f8a22b7ba944e9d107bc3a55b13b862ed21661
Victor Zhukov <v.zhukov@simplex48.ru>
2023-03-17 13:15:37 +0300

    Создали контроллер CRUD

---
+++ SportsStore/SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public sealed class AdminController : Controller
+    {
+        private IProductRepository m_repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            m_repository = repository;
+        }
+
+        public ViewResult Index() => View(m_repository.Products);
+    }
+}
