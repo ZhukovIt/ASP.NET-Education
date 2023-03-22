73098dc9b368c60eefcd2632f9abb11f587b3bf6
Victor Zhukov <v.zhukov@simplex48.ru>
2023-03-22 12:54:48 +0300

    Добавили проверку достоверности модели при редактировании товаров

--- SportsStore/SportsStore/Models/Product.cs
+++ SportsStore/SportsStore/Models/Product.cs
@@ -1,10 +1,23 @@
-﻿namespace SportsStore.Models {
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SportsStore.Models {
 
     public class Product {
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите наименование товара")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите описание товара")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue,
+            ErrorMessage = "Пожалуйста, введите положительное значение для цены товара")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, укажите категорию товара")]
         public string Category { get; set; }
     }
 }
