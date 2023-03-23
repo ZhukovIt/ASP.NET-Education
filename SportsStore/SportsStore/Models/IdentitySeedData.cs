31426d1f40406c197d96035ad0bd44b110cc15d5
Victor Zhukov <v.zhukov@simplex48.ru>
2023-03-23 11:56:07 +0300

    Создали защиту для средств администрирования

---
+++ SportsStore/SportsStore/Models/IdentitySeedData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    public static class IdentitySeedData
+    {
+        private const string ADMIN_USER = "Admin";
+        private const string ADMIN_PASSWORD = "Secret123$";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            UserManager<IdentityUser> userManager = app.ApplicationServices
+                .GetRequiredService<UserManager<IdentityUser>>();
+            IdentityUser user = await userManager.FindByIdAsync(ADMIN_USER);
+            if (user == null)
+            {
+                user = new IdentityUser("Admin");
+                await userManager.CreateAsync(user, ADMIN_PASSWORD);
+            }
+        }
+    }
+}
